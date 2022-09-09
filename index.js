73c88eae5bfa4bab675fd8cd7e28c9616c4a5d30
coolcoolcoolyo <alexjspagnoli@gmail.com>
2022-09-09 04:21:03 -0400

    commit tree b9ce7ba704d1e40f82c05f47aa0a4ee905931438

---
+++ index.js
@@ -0,0 +1,24 @@
+function introduction(name = "Aki") {
+    let name = "Aki";
+    console.log("Hi, my name is ${name}.");
+}
+
+function logTwoValues(value1, value2) {
+    console.log('The two values are ${value1} and ${value2}.');
+}
+
+function introductionWithLanguage(name, language) {
+    console.log("Hi, my name is ${name} and I am learning to program in ${language}.");
+}
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+ 
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
+  
+function introductionWithLanguageOptional(name, JavaScript) {
+    console.log('The two values are ${name}} and ${language}.');
+}
